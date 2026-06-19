161f0ae541e07d0bba62b4c0518e45ee170985f9
anubrr21 <anubratabhattacharyya81@gmail.com>
2026-06-19 19:10:39 +0530

    Add jwt secret expiration cmd

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -9,7 +9,7 @@ import catchAsync from '../utils/catchAsync.js';
  */
 const signToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+    expiresIn: process.env.JWT_EXPIRES_IN || '7d'
   });
 };
 
